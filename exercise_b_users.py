4409c95107dd268608f6a58b49540007dd7def27
belus90 <belus90@gmail.com>
2022-02-23 10:13:01 +0000

    Update_hom

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -1,3 +1,6 @@
+from http.client import UnimplementedFileMode
+
+
 users = {
   "Jonathan": {
     "twitter": "jonnyt",
@@ -55,28 +58,47 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# print(users["Jonathan"]["twitter"])
+print(users["Jonathan"]["twitter"])
 
 # 2. Get Erik's hometown
-# print(users["Erik"]["home_town"])
+print(users["Erik"]["home_town"])
 
 # 3. Get the array of Erik's lottery numbers
-# print(users["Erik"]["lottery_numbers"])
+print(users["Erik"]["lottery_numbers"])
 
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
-# new_element = users["Avril"]["pets"]
-# print(users["Avril"]["pets"].get("name"))
-# print (new_element[0])
 
 # 5. Get the smallest of Erik's lottery numbers
+lottery_num = users["Erik"]["lottery_numbers"]
+print(min(lottery_num))
 
 # 6. Return an array of Avril's lottery numbers that are even
+lot_num = users["Avril"]["lottery_numbers"]
+for num in lot_num:
+  if num %2 == 0:
+    print (num)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+lottery_num.append(7)
+print(lottery_num)
 
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["hometown"] = "Edinburgh"
+print(users["Erik"]["hometown"])
 
 # 9. Add a pet dog to Erik called "Fluffy"
+new_dog = {"name": "Fluffy", "species": "dog"}
+new_pet = users["Erik"]["pets"]
+new_pet.append(new_dog)
+print(new_pet)
 
 # 10. Add another person to the users dictionary
+new_user = { "Mate": {
+    "twitter": "belus90",
+    "lottery_numbers": [12, 16, 3, 2, 6, 90],
+    "home_town": "Gyor"
+}
+}
+users.update(new_user)
+print(users)
--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,10 +3,15 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-
+for num in numbers:
+    if num % 2 == 0:
+        print (num)
 
 # 2. Print the difference between the largest and smallest value:
-
+min_num = min(numbers)
+max_num = max(numbers)
+diff = max_num - min_num
+print (diff)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 
